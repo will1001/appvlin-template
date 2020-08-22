e534b043de7d752dc7321724e11ba1546ddf0db7
wili rahmat muhamad <wilirahmatm@gmail.com>
2020-08-22 10:42:43 +0800

    responsive code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,8 @@
+$('.button').click(function(e)
+{
+    e.preventDefault();
+});
+
 if ($('.banner-slider').length) {
       $('.banner-slider').owlCarousel({
         center: true,
@@ -35,4 +40,64 @@ for (let i = 0; i < button.length; i++) {
     }, false);
 }
     
-    
+
+$('.partner').owlCarousel({
+      loop: true,
+      margin: 0,
+      responsiveClass: true,
+      nav: false,
+      dots: false,
+      autoplay: true,
+      autoplayTimeout: 1000,
+      autoplayHoverPause: true,
+      slideTransition:'linear',
+      responsive:{
+          0:{
+              items:2,
+          },
+          480:{
+              items:3,
+          },
+          768:{
+              items:5,
+          }
+      }
+})
+
+$('.ss-group').owlCarousel({
+      loop: true,
+      margin: 0,
+      responsiveClass: true,
+      nav: false,
+      dots: false,
+      autoplayTimeout: 1000,
+      autoplayHoverPause: true,
+        animateIn: 'Slide',
+          responsive:{
+          0:{
+              items:2,
+          },
+          575:{
+              items:3,
+          },
+          992:{
+              items:5,
+          }
+      }
+})
+
+// faq
+
+$('.faq .accordion-button .title').on('click', function (e) { 
+    var element = $(this).parent('.faq .accordion-button');
+    element.hasClass('open') ? element.removeClass("open") : element.addClass("open")
+    // // element.hasClass('open') ? element.find('.content').slideUp(300, "swing") : element.find('.content').slideDown(300, "swing")
+    // if (element.hasClass('open')) {
+    //     element.removeClass("open")
+    //     element.find('.content').slideUp(300, "swing")
+    // } else {
+    //     element.addClass("open")
+    //     element.find('.content').slideDown(300, "swing")
+    // }
+})
+
