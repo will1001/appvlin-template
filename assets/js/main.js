5ac2f38c99bf448b335c59ab45349591ffd8a40c
wili rahmat muhamad <wilirahmatm@gmail.com>
2020-09-01 10:14:35 +0800

    scroll to top button

--- assets/js/main.js
+++ assets/js/main.js
@@ -155,3 +155,20 @@ $('.app-statistic h3').countUp({
   $(window).on('load', function () {
     $('.preloader').fadeOut(1000);
   });
+
+  // Scroll To Top 
+    var scrollTop = $(".scrollToTop");
+    $(window).on('scroll', function () {
+      if ($(this).scrollTop() < 500) {
+        scrollTop.removeClass("active");
+      } else {
+        scrollTop.addClass("active");
+      }
+    });
+    //Click event to scroll to top
+    $('.scrollToTop').on('click', function () {
+      $('html, body').animate({
+        scrollTop: 0
+      }, 500);
+      return false;
+    });
